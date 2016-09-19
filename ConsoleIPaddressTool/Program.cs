173a8bb576b913584e431fb02b6b3daa4688a458
Daniel Bowen <danielrbowen@live.com>
2016-09-19 15:36:29 -0600

    Got to finish up BinaryStringAddressToDottedDecimal by adding zeros if not 32 characters long

--- ConsoleIPaddressTool/Program.cs
+++ ConsoleIPaddressTool/Program.cs
@@ -56,11 +56,13 @@ namespace Learning
             //The whole address in binary
             string addressBinary = addressDottedBinary[0] + addressDottedBinary[1] + addressDottedBinary[2] + addressDottedBinary[3];
 
+            // WRONG
             // The network address is n bits where n is the prefix of CIDR notation
-            var networkAddress = addressBinary.Substring(0, prefix);
-
+            //var networkAddress = addressBinary.Substring(0, prefix);
+            
+            // WRONG
             // The host address is (32 - n)
-            var hostAddress = addressBinary.Substring(prefix, 32 - prefix);
+            //var hostAddress = addressBinary.Substring(prefix, 32 - prefix);
 
             // The network mask is 1's for each bit n times with a trail of zeros at the end
             var networkMask = "";
@@ -84,8 +86,10 @@ namespace Learning
                 broadcastAddress += "1";
             }
 
-            // The First Address is (any address) (bitwise AND) (network mask)
-            int firstAddressInt = BinaryStringToInt(addressBinary) & BinaryStringToInt(networkMask);
+            // The Network Address is (any address) (bitwise AND) (network mask)
+            var networkAddressInt = BinaryStringToInt(addressBinary) & BinaryStringToInt(networkMask);
+            var firstAddressInt = networkAddressInt - 2;
+            string networkAddressBinary = Convert.ToString(firstAddressInt, 2);
             string firstAddressBinary = Convert.ToString(firstAddressInt, 2);
 
             // The Last Address is (any address) (bitwise OR) [NOT(network mask)]
@@ -96,16 +100,17 @@ namespace Learning
             int totalAddresses = (int)Math.Pow(2, (32 - prefix));
 
             // The number of usable addresses is 
-            int usableAddresses = (32 - prefix) + 1 - 2;
+            //int usableAddresses = (32 - prefix) + 1 - 2;
+            int usableAddresses = totalAddresses - 2;
 
-            //Total addresses granted is (32-n in decimal) + 1 - 2
+            //Total addresses granted is (32-n in decimal) + 1 - 2. Is total addresses?
 
             //prefix length for number of subnets is ?
             // subnet prefix = prefix + log base 2 (number of subnets (power of 2))
             // Read for subnets
             Console.Write("Enter the number of subnets: ");
             int subnets = 0;
-            int.TryParse(Console.ReadLine(), out subnets);
+            //int.TryParse(Console.ReadLine(), out subnets);
             int subnetPrefixBits = 0;
             if (subnets > 0)
             {
@@ -123,26 +128,118 @@ namespace Learning
             // 2 ^ (subnetPrefix - prefix) = subnets
             int subnetPrefixLength = 0;
             Console.Write("Enter the subnet prefix length: ");
-            int.TryParse(Console.ReadLine(), out subnetPrefixLength);
+            //int.TryParse(Console.ReadLine(), out subnetPrefixLength);
             int maximumSubnets = (int)Math.Pow(2, (subnetPrefixLength - prefix));
 
             // The subnet broadcast address (32-n) bits to decimal + the zero (1) - 2 (first bit and last bit for special addresses)
-
+            string oneTwo = "00000001001000110100010101100111";
+            Console.WriteLine("\n" + oneTwo.Substring(0, 8));
+            Console.WriteLine(oneTwo.Substring(8, 8));
+            Console.WriteLine(oneTwo.Substring(16, 8));
+            Console.WriteLine(oneTwo.Substring(24, 8));
             Console.WriteLine("Prefix: {0}", prefix.ToString());
-            Console.WriteLine("Address in Binary: {0}", addressBinary);
-            Console.WriteLine("Network Address: {0}", networkAddress);
-            Console.WriteLine("Host Address: {0}", hostAddress);
-            Console.WriteLine("Network Mask: {0}", networkMask);
-            Console.WriteLine("Broadcast Address: {0}", broadcastAddress);
-            Console.WriteLine("First Address: {0}", firstAddressBinary);
-            Console.WriteLine("Last Address: {0}", lastAddressBinary);
+            Console.WriteLine("Address: {0} {1} {2}", addressBinary, BinaryStringToHex(addressBinary), BinaryStringToInt(addressBinary));
+            //Console.WriteLine("Network Address: {0} {1} {2} {3}", networkAddressBinary, BinaryStringToHex(networkAddressBinary), BinaryStringToInt(networkAddressBinary), BinaryStringToDottedDecimal(networkAddressBinary));
+            //Console.WriteLine("Host Address: {0} {1} {2}", hostAddress, BinaryStringToHex(hostAddress), BinaryStringToInt(hostAddress));
+            Console.WriteLine("Network Mask: {0} {1} {2}", networkMask, BinaryStringToHex(networkMask), BinaryStringToInt(networkMask));
+            Console.WriteLine("Broadcast Address: {0} {1} {2}", broadcastAddress, BinaryStringToHex(broadcastAddress), BinaryStringToInt(broadcastAddress));
+            Console.WriteLine("First Address: {0} {1} {2}", firstAddressBinary, BinaryStringToHex(firstAddressBinary), BinaryStringToInt(firstAddressBinary));
+            Console.WriteLine("Last Address: {0} {1} {2}", lastAddressBinary, BinaryStringToHex(lastAddressBinary), BinaryStringToInt(lastAddressBinary));
             Console.WriteLine("Total Addresses: {0}", totalAddresses);
             Console.WriteLine("Usable Addresses: {0}", usableAddresses);
-            Console.WriteLine("Subnet Prefix Length: {0}", subnetPrefixBits);
+            Console.WriteLine("Subnet Prefix Length for given subnets: {0}", subnetPrefixBits);
 
             Console.ReadKey();
         }
 
+        public static string BinaryStringAddresssToDottedDecimal(string binaryString)
+        {
+            string result = "";
+            result += BinaryStringToInt(binaryString.Substring(0, 8));
+            result += ".";
+            result += BinaryStringToInt(binaryString.Substring(8, 8));
+            result += ".";
+            result += BinaryStringToInt(binaryString.Substring(16, 8));
+            result += ".";
+            result += BinaryStringToInt(binaryString.Substring(24, 8));
+            return result;
+        }
+
+        public static string BinaryStringToHex(string binaryString)
+        {
+            string result = "";
+            if (!(binaryString.Length % 4 == 0))
+            {
+                for(int index = binaryString.Length; !(index % 4 == 0); index++)
+                {
+                    binaryString = binaryString.Insert(0, "0");
+                }
+            }
+
+
+            for (int index = 0; index < binaryString.Length; index += 4)
+            {
+                string substring = binaryString.Substring(index, 4);
+                char add = '0';
+                switch (substring)
+                {
+                    case "0000":
+                        add = '0';
+                        break;
+                    case "0001":
+                        add = '1';
+                        break;
+                    case "0010":
+                        add = '2';
+                        break;
+                    case "0011":
+                        add = '3';
+                        break;
+                    case "0100":
+                        add = '4';
+                        break;
+                    case "0101":
+                        add = '5';
+                        break;
+                    case "0110":
+                        add = '6';
+                        break;
+                    case "0111":
+                        add = '7';
+                        break;
+                    case "1000":
+                        add = '8';
+                        break;
+                    case "1001":
+                        add = '9';
+                        break;
+                    case "1010":
+                        add = 'A';
+                        break;
+                    case "1011":
+                        add = 'B';
+                        break;
+                    case "1100":
+                        add = 'C';
+                        break;
+                    case "1101":
+                        add = 'D';
+                        break;
+                    case "1110":
+                        add = 'E';
+                        break;
+                    case "1111":
+                        add = 'F';
+                        break;
+                    default:
+                        throw new Exception("BinaryStringToHex has a bug");
+                }
+
+                result += add;                
+            }
+            result = result.Insert(0, "0x");
+            return result;            
+        }
 
         /// <summary>
         /// Converts an array of integers to binary strings
@@ -156,7 +253,7 @@ namespace Learning
             for (int index = 0; index < arrayLength; ++index)
             {
                 result[index] = Convert.ToString(array[index], 2);
-            }
+            }            
             return result;
         }
 
@@ -168,15 +265,17 @@ namespace Learning
         public static int BinaryStringToInt(string binaryString)
         {
             int result = 0;
-            for (int index = 0; index < binaryString.Length; index++)
-            {
-                char binaryStringIndex = binaryString[index];
-                if (binaryStringIndex == '1')
+            int positionFromRight = binaryString.Length - 1;
+            for (int index = 0; index < binaryString.Length ; index++)
+            {                
+                char binaryStringIndexValue = binaryString[index];
+                if (binaryStringIndexValue == '1')
                 {
                     int intAdd = 0;
-                    int.TryParse(Math.Pow(2, index).ToString(), out intAdd);
-                    result += intAdd;
+                    int.TryParse(Math.Pow(2, positionFromRight).ToString(), out intAdd);
+                    result += intAdd;                    
                 }
+                positionFromRight--;
             }
             return result;
         }
